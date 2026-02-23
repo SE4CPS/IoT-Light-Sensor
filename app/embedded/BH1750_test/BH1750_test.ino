04b8dca6bead255ef1bbd9f137a8ac51f57fe188
nmontoya2-ux <n_montoya2@u.pacific.edu>
2026-02-23 14:19:06 -0800

    Add BH1750 sensor test sketch and update README

---
+++ app/embedded/BH1750_test/BH1750_test.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 lightMeter;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(21, 22);  // SDA, SCL
+
+  if (lightMeter.begin()) {
+    Serial.println("BH1750 started!");
+  } else {
+    Serial.println("Error initializing BH1750");
+  }
+}
+
+void loop() {
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  delay(1000);
+}
